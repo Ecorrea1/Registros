2e1ba407a848ae9a7b6c332743d7a067d2598fa8
Emmanuel Correa <ebcorreac@gmail.com>
2022-08-09 03:29:44 -0400

    changes in modal

--- js/index.js
+++ js/index.js
@@ -15,3 +15,20 @@ const submitNewData = async (data) => {
     const data = await response.json();
     return data;
 }
+
+const exampleModal = document.getElementById('myModal')
+exampleModal.addEventListener('show.bs.modal', event => {
+  // Button that triggered the modal
+  const button = event.relatedTarget
+  // Extract info from data-bs-* attributes
+  const recipient = button.getAttribute('data-bs-whatever')
+  // If necessary, you could initiate an AJAX request here
+  // and then do the updating in a callback.
+  //
+  // Update the modal's content.
+  const modalTitle = exampleModal.querySelector('.modal-title')
+  const modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+  modalTitle.textContent = `New message to ${recipient}`
+  modalBodyInput.value = recipient
+})
