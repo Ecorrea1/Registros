ca5f6fac32aa5135b046cb22a052b0750fa8b727
Emmanuel Correa <ebcorreac@gmail.com>
2022-08-12 03:00:07 -0400

    agregando cambios

--- js/index.js
+++ js/index.js
@@ -1,22 +1,37 @@
-const api = 'https://montecarlos-register.herokuapp.com/api/';
+const modeDevelop = false; 
+const api = modeDevelop ? 'http://192.168.1.15:3000/api/' : 'https://montecarlos-register.herokuapp.com/api/';
 
-const consulta = (url) => {
-  return new Promise((resolve, reject) => {
-      const requestOptions = { method: 'GET', redirect: 'follow' };
-
-      fetch(url, requestOptions)
-          .then(response => response.json())
-          .then(data => { resolve(JSON.parse(JSON.stringify(data))); })
-          .catch(err => console.log(err))
+function consulta  ( url ) {
+  return new Promise(( resolve, reject ) => {
+    const requestOptions = { method: 'GET', redirect: 'follow' };
+    console.log(`${url}`);
+    fetch( url, requestOptions )
+    .then( response => response.json() )
+    .then( data => { resolve( JSON.parse( JSON.stringify( data ) ) ); })
+    .catch( err => console.log( err ) )
   });
 }
 
-const allRegisters = async () => {
-  const registers = await consulta(api + 'registers');
-  console.log('registers', registers);
-  return registers;
+// Show all registers in the table
+const show_registers = async () => {
+  const registers = await consulta( api + 'registers');
+  imprimirLista( registers.data );
 }
 
+
+async function imprimirLista( data ) {
+  
+  document.getElementById("list_row").innerHTML ="";
+  for (const i in data ) {
+      const { id, name, age, phone, total, payment, balance, cristal, treatment, frame, observation, professional, date_attention, created_at, updated_at } = data[i];
+      let rowClass  = 'text-right';
+      let customRow = `<td>${ [ id, name, age, phone, total, payment, balance, cristal, treatment, frame, observation, professional, date_attention, created_at, updated_at ].join('</td><td>') }</td>`;
+      let row       = `<tr class="${ rowClass }">${ customRow }</tr>`;
+      document.getElementById("list_row").innerHTML += row;
+  }
+}
+
+show_registers();
 
 const exampleModal = document.getElementById('myModal')
 exampleModal.addEventListener('show.bs.modal', event => {
