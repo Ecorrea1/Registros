ef7067f534243ad3d885365ec52d1d0b2aa1915b
Emmanuel Correa <ebcorreac@gmail.com>
2023-09-22 19:06:17 -0300

    trying

--- js/connection.js
+++ js/connection.js
@@ -1,2 +1,2 @@
 const developerMode = false;
-const api = developerMode  ? 'http://192.168.1.16:3000/api/' : 'https://registersapi.onrender.com/api/';
+let api = developerMode  ? 'http://192.168.1.16:3000/api/' : 'https://registersapi.onrender.com/api/';
