acab4495d006117509f78e1eb1ef8051bb001c21
Emmanuel Correa <ebcorreac@gmail.com>
2023-12-14 00:08:03 -0300

    [FIX] Connection

--- js/config/connection.js
+++ js/config/connection.js
@@ -1,4 +1,4 @@
-const developerMode = true;
+const developerMode = false;
 let api = developerMode  ? 'http://localhost:3000/api/' : 'https://registersapi.onrender.com/api/';
 const url = origin === "http://127.0.0.1:5500" || origin.includes('http://192.168.1.') ? "" : "/registros";
 
